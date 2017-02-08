33e1ab0d8c913516d3865ca23dd5e9d380423c31
Morgan <mpeter15@hawk.iit.edu>
2017-02-08 16:26:26 -0600

    Tokenssssssss

--- lexical.py
+++ lexical.py
@@ -1,1 +1,29 @@
-import
+import ply.lex as lex
+
+# Token list
+tokens = (
+    'RULE',
+    'NUMBER',
+    'LOGIC',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'POW',
+    'LTHAN',    # LESSTHAN <
+    'LETHAN',   # LESSTHANOREQUALTO <=
+    'GTHAN',    # GREATERTHAN >
+    'GETHAN',   # GREATERTHANOREQUALTO >=
+    'ETO',      # EQUALTO ==
+    'TYPE',
+    'START',
+    'EQUALS',
+    'VAR',
+    'ARROW',
+    'MATCH',
+    'MATCHBREAK',
+    'EOL',      # END OF LINE
+    'IF'
+)
+
+# TYPES

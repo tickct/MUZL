2b994bdf6c5a0835d8325a364778483e59b04061
Morgan <mpeter15@hawk.iit.edu>
2017-02-08 21:42:36 -0600

    Tokens

--- lexical.py
+++ lexical.py
@@ -2,30 +2,34 @@ import ply.lex as lex
 
 # Token list
 tokens = (
-    'RULE',
-    'NUMBER',
-    'LOGIC',
-    'PLUS',
-    'MINUS',
-    'TIMES',
-    'DIVIDE',
+    'RULE', 'NUMBER', 'LOGIC',
+    'PLUS', 'MINUS', 'TIMES', 'DIVIDE',
     'POW',
     'LTHAN',    # LESSTHAN <
     'LETHAN',   # LESSTHANOREQUALTO <=
     'GTHAN',    # GREATERTHAN >
     'GETHAN',   # GREATERTHANOREQUALTO >=
     'ETO',      # EQUALTO ==
-    'TYPE',
-    'START',
-    'EQUALS',
-    'VAR',
-    'ARROW',
-    'MATCH',
+    'TYPE', 'START', 'EQUALS',
+    'VAR', 'ARROW', 'MATCH',
     'MATCHBREAK',
     'EOL',      # END OF LINE
-    'COMMA',
-    'BRACKET',
-    'IF'
+    'COMMA', 'BRACKET', 'IF'
 )
 
+# regular expressions
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_EQUALS = r'='
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+
 # TYPES
+
+
+# Main
+if __name__ == '__main__':
+     lex.runmain()

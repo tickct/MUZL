4ab35f61e94b0591e26be966e174856e5c71a3c7
Morgan <mpeter15@hawk.iit.edu>
2017-02-10 22:29:16 -0600

    added much more to it. Read some of my comments was a tad confused...

--- lexical.py
+++ lexical.py
@@ -2,77 +2,100 @@
 MORGAN PETERS AND SEAN GRAY
 2/9/2017
 Yo i've been looking at this py doc more and i think this should work...
+
+http://www.slideshare.net/dabeaz/writing-parsers-and-compilers-with-ply
 """
 
 import ply.lex as lex
 
+# reserved words...
+reserved = {
+    'if': 'IF',
+    'rule': 'RULE',
+    'start': 'START',
+    'match': 'MATCH',
+    'int': 'INT',
+    'float': 'FLOAT',
+}
+
 # Token list
-tokens = (
-    'RULE', 'NUMBER', 'LOGIC',
-    'PLUS', 'MINUS', 'TIMES', 'DIVIDE',
-    'POW',
+tokens_list = [
+    'LOGIC',
+    'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'POW',
     'LTHAN',    # LESSTHAN <
     'LETHAN',   # LESSTHANOREQUALTO <=
     'GTHAN',    # GREATERTHAN >
     'GETHAN',   # GREATERTHANOREQUALTO >=
     'ETO',      # EQUALTO ==
-    'TYPE', 'START', 'EQUALS',
-    'VAR', 'ARROW', 'MATCH',
+    'TYPE','EQUALS',
+    'VAR', 'ARROW',
     'MATCHBREAK',
     'EOL',      # END OF LINE
     'COMMA', 'LBRACKET', 'RBRACKET', 'IF'
-)
+]
 
-# regular expressions
-t_MATCHBREAK = r'\|'
-t_COMMA = r'\,'
-t_LBRACKET = r'\['
-t_RBRACKET = r'\]'
-t_LETHAN = r'\<='
-t_LTHAN = r'\<'
-t_GETHAN = r'\>='
-t_GTHAN = r'\>'
-t_PLUS = r'\+'
-t_ARROW = r'->' # longer expressions must go first
-t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIVIDE = r'/'
-t_POW = r'\^'
-t_ETO = r'=='
-t_EQUALS = r'='
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
+# it's weird it says tokens should be a list but then uses ()
+# in some :/ I just used []
+tokens = tokens_list + list(reserved.values())
 
 
-# action code of regular expressions
+# I also didn't know if you wanted to do this as a class a def or just regular
+# the notes on ply has options for all three so it doesn't really matter
+def mUZLLexer():
+    # regular expressions
+    t_MATCHBREAK = r'\|'
+    t_COMMA = r'\,'
+    t_LBRACKET = r'\['
+    t_RBRACKET = r'\]'
+    t_LETHAN = r'\<='
+    t_LTHAN = r'\<'
+    t_GETHAN = r'\>='
+    t_GTHAN = r'\>'
+    t_PLUS = r'\+'
+    t_ARROW = r'->'     # longer expressions must go first
+    t_MINUS = r'-'
+    t_TIMES = r'\*'
+    t_DIVIDE = r'/'
+    t_POW = r'\^'
+    t_ETO = r'=='
+    t_EQUALS = r'='
+    t_LPAREN = r'\('
+    t_RPAREN = r'\)'
 
-def t_FLOAT(t):
-    r'-?\d+\.d*(e-?\d+)?'
-    t.value = float(t.value)
-    return t
+    # action code of regular expressions
 
+    def t_FLOAT(t):
+        r'-?\d+\.d*(e-?\d+)?'
+        t.value = float(t.value)
+        return t
 
-def t_INT(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
+    def t_INT(t):
+        r'\d+'
+        t.value = int(t.value)
+        return t
 
+    def t_COMMENT(t):
+        r'\[#].'
+        pass
 
-def t_comments(t):
-    r'[#][^\n]*'
-    pass
+    # end of line; syntax the same as the example
+    def t_EOL(t):
+        r'!+'
+        t.lexer.lineno += len(t.value)
 
+    # A string containing ignored characters (spaces and tabs)
+    t_ignore = ' \t'
 
-# end of line; syntax the same as the example
-def t_EOL(t):
-    r'!+'
-    t.lexer.lineno += len(t.value)
+    # literals? like for [], ! ect.. do we need these?
 
+    # Error handling rule
+    def t_error(t):
+        print("Illegal character '%s'" % t.value[0])
+        t.lexer.skip(1)
 
-# reserved words...b
-# precedence
+    # precedence (i think this is the second part of the project?)
 
-# build it
+    # build it
 
 # Main
 if __name__ == '__main__':

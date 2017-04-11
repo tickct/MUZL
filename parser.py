86e5c674c5cc41a26daa510b7cfecb4b656c68cb
Morgan <mpeter15@hawk.iit.edu>
2017-04-11 17:25:25 -0500

    removed ast. added error

--- parser.py
+++ parser.py
@@ -6,44 +6,12 @@ use ast -> pythons abstract syntax tree function
 """
 
 import ply.yacc as yacc
-import ast
 # token map import
 from lexical import tokens
 
-# starting symbol
-# start = 'fact'
 
-
-# name rule ( arg1 type; arg2 type ) -> type = (X + Y)!
-def p_funcrule(p):
-    'funcrule: NAME RULE (parameters) ARROW TYPE EQUALS'
-    p[0] = ast.FunctionDef(None, p[1], tuple(p[3]), (), 0, None, p[5])
-
-
-# parameters ( [vararglist] )
-def p_parameters(p):
-    '''parameters : LPAREN RPAREN
-                | LPAREN vararglist RPAREN'''
-    if len(p) == 3:
-        p[0] = []
-    else:
-        p[0] = p[2]
-
-
-# vararglist:
-def p_vararglist(p):
-    '''vararglist: vararglist COMMA NAME TYPE
-                | NAME TYPE'''
-    pass
-
-
-binary_ops = {
-    "+": ast.Add,
-    "-": ast.Sub,
-    "*": ast.Mult,
-    "/": ast.Div,
-}
-
+def p_error(p):
+    print("Syntax Error at", p.value)
 
 precedence = (
     ('nonassoc', 'LTHAN', 'GTHAN', 'ETO', 'GETHAN', 'LETHAN'),
@@ -59,5 +27,7 @@ def make_parser():
 
 
 if __name__ == "__main__":
-    make_parser()
+    parser = make_parser()
+    print(parser.parse("123"))
+
 

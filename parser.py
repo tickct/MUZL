5de143b52b4551788553edd074d0b8eb59d11035
Sean Gray <seangrayiit@gmail.com>
2017-04-12 11:26:43 -0500

    can add and convert to hex

--- parser.py
+++ parser.py
@@ -11,18 +11,48 @@ import ply.yacc as yacc
 from lexical import tokens
 
 
-def p_binary_operators(p):
+"""def p_binary_operators(p):
     '''expression : expression PLUS expression
                   | expression MINUS expression
                   | expression TIMES expression
                   | expression DIVIDE expression
                   | LPAREN expression RPAREN
                   | val'''
+    print(x for x in p)
+    if (len(p) == 4) :
+        if p[2] == '+':
+            print(p)
+            p[0] = p[1] + p[3]
+        if p[2] == '-':
+            p[0] = p[1] + p[2]
+        if p[2] == '*':
+            p[0] = p[1] + p[2]
+        if p[2] == '/':
+            p[0] = p[1] + p[2]
+        if p[1] == '(':
+            p[0] = p[2]
+"""
+def p_expression_plus(p):
+    'expression : expression PLUS val'
+    p[0] = p[1] + p[3]
+    
+def p_expression_val(p):
+    'expression : val'
+    p[0] = p[1]
+    
 def p_type_change(p):
-    '''val : VAR DOT type
+    '''val : VAR type
            | VAR
-           | value DOT type
+           | value type
            | value '''
+    print(p[1])
+    if (len(p) == 2):
+        p[0] = p[1]
+    elif(p[2] == 'hex'):
+        print('hexing')
+        p[0] = hex(int(p[1]))
+    else:
+        print('not hexing')
     
 def p_value(p):
     '''value : INT
@@ -31,7 +61,7 @@ def p_value(p):
              | STRING
              | BOOL
              | HEX '''
-    
+    p[0] = p[1]
 def p_type(p):
     '''type : INT_T
             | FLOAT_T
@@ -39,10 +69,11 @@ def p_type(p):
             | STRING_T
             | BOOL_T
             | HEX_T '''
+    p[0] = p[1]
     
 def p_error(p):
     print("Syntax Error at", p.value)
-
+   
 
 precedence = (
     ('nonassoc', 'LTHAN', 'GTHAN', 'ETO', 'GETHAN', 'LETHAN'),
@@ -52,14 +83,15 @@ precedence = (
     )
 
 
-def make_parser():
-    yaccparser = yacc.yacc()
-    return yaccparser
+parser = yacc.yacc()
 
-
-if __name__ == "__main__":
-    yacc = make_parser()
-
-    print(yacc.parse("12 * 12"))
+while True:
+    try:
+        s = input('!-')
+    except EOFError:
+        break
+    if not s: continue
+    result = parser.parse(s)
+    print(result)
 
 
